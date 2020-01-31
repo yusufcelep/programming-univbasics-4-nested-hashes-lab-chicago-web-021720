a0563f9607e3a00c5bc37878bb38d6bd5fa84a3a
Yusuf Celep <yusufcelep8@gmail.com>
2020-01-31 21:14:35 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -78,8 +78,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-     programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-     programmer_hash[:yukihiro_matsumoto][:languages] = "LISP", "C"
+     programmer_hash.merge(:yukihiro_matsumoto => {:known_for => "Ruby", :languages => ["LISP", "C"]})
 end
 
 def changing_alan
